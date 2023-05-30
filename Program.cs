8547d1767eab009ad42ea593e853d64f99216168
Nathan Rocha Torrente <nathan.torrente.rocha@gmail.com>
2023-05-30 15:26:59 -0400

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
-﻿using System;
-
-class programa
+﻿
+try
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("\n Informa o primeiro numero: ");
-        int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("\n Informa o primeiro numero: ");
+    int a = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("\n Informa o primeiro numero: ");
-        int b = int.Parse(Console.ReadLine());
+    Console.WriteLine("\n Informa o primeiro numero: ");
+    int b = int.Parse(Console.ReadLine());
 
-        int result = a / b;
-        Console.WriteLine(result);
-    }
+    int result = a / b;
+    Console.WriteLine(result);
+
+
 }
+catch(Exception erros)
+{
+    Console.WriteLine("Algo esta mution erradu");
+}
+
+
 
 
