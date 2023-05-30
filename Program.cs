8910bdeaf40b0531c45bcab1bcef1bef09d50a11
Nathan Rocha Torrente <nathan.torrente.rocha@gmail.com>
2023-05-30 15:03:17 -0400

    Inicio do projeto de divisão de dois números

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n Informa o primeiro numero: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n Informa o primeiro numero: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int result = a / b;
+        Console.WriteLine(result);
+    }
+}
+
+
